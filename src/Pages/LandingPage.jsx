60688c626ace44a5a6f1574431f538e52605acc8
Diego Castillo <dcastillo10@ucol.mx>
2024-05-27 20:17:09 -0600

    Modificaciones carrusel

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -10,31 +10,77 @@ const LandingPage = () => {
     <>
       <Home />
       <div className="recipe-cards">
-        <Carousel>
+        <Carousel
+          controls={true}
+          indicators={false}
+          interval={null}
+          className="custom-carousel"
+        >
           <Carousel.Item>
-            <div className="d-flex justify-content-center align-items-center text-white">
+            <div className="d-flex justify-content-center">
               <RecipeCard
                 img="/src/assets/recipe1.jpg"
                 title="Cheesecake"
                 description="Creamy, rich cheesecake crowned with blueberry"
               />
+              <RecipeCard
+                img="/src/assets/recipe2.jpg"
+                title="Chocolate muffins"
+                description="Loaded with chocolate chips in every single bite."
+              />
+              <RecipeCard
+                img="/src/assets/recipe3.jpg"
+                title="Spaghetti bolognese"
+                description="A true Italian classic with a meaty, chilli sauce."
+              />
+              <RecipeCard
+                img="/src/assets/recipe4.jpg"
+                title="Grilled steak"
+                description="Juicy steak with a nice crust and big enough to slice and serve family."
+              />
+              <RecipeCard
+                img="/src/assets/recipe5.jpg"
+                title="Chicken breast"
+                description="These juicy, tender chicken breasts are delicious."
+              />
             </div>
           </Carousel.Item>
           <Carousel.Item>
-            <div className="d-flex justify-content-center align-items-center text-white">
+            <div className="d-flex justify-content-center">
               <RecipeCard
                 img="/src/assets/recipe1.jpg"
                 title="Cheesecake"
                 description="Creamy, rich cheesecake crowned with blueberry"
               />
-            </div>
-          </Carousel.Item>
-          <Carousel.Item>
-            <div className="d-flex justify-content-center align-items-center text-white">
               <RecipeCard
-                img="/src/assets/recipe1.jpg"
-                title="Cheesecake"
-                description="Creamy, rich cheesecake crowned with blueberry"
+                img="/src/assets/recipe2.jpg"
+                title="Chocolate muffins"
+                description="Loaded with chocolate chips in every single bite."
+              />
+              <RecipeCard
+                img="/src/assets/recipe3.jpg"
+                title="Spaghetti bolognese"
+                description="A true Italian classic with a meaty, chilli sauce."
+              />
+              <RecipeCard
+                img="/src/assets/recipe4.jpg"
+                title="Grilled steak"
+                description="Juicy steak with a nice crust and big enough to slice and serve family."
+              />
+              <RecipeCard
+                img="/src/assets/recipe5.jpg"
+                title="Chicken breast"
+                description="These juicy, tender chicken breasts are delicious."
+              />
+              <RecipeCard
+                img="/src/assets/recipe5.jpg"
+                title="Chicken breast"
+                description="These juicy, tender chicken breasts are delicious."
+              />
+              <RecipeCard
+                img="/src/assets/recipe5.jpg"
+                title="Chicken breast"
+                description="These juicy, tender chicken breasts are delicious."
               />
             </div>
           </Carousel.Item>
